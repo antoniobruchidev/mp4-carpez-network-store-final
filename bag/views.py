25c647185df88b4d2fec9c7a5f161c8dbd7dd5b1
Antonio Bruchi <antoniobruchi@hotmail.com>
2024-10-17 17:02:51 +0100

    Feat: add bag to toast success

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
+
+from products.models import Product
+from django.contrib import messages
 
 
 def view_bag(request):
@@ -10,6 +13,7 @@ def view_bag(request):
 def add_to_bag(request, product_id):
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
+    product = get_object_or_404(Product, pk=product_id)
 
     if product_id in list(bag.keys()):
         bag[product_id] += 1
@@ -17,4 +21,5 @@ def add_to_bag(request, product_id):
         bag[product_id] = 1
 
     request.session['bag'] = bag
+    messages.success(request, f"Successfully added {product.name}")
     return redirect(redirect_url)
