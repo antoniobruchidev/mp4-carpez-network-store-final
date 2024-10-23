eb4cd9224a9feb7f8fe6b91896c96a7d852600e1
Antonio Bruchi <antoniobruchi@hotmail.com>
2024-10-23 09:07:49 +0100

    Maint: trying to override allauth automatic email system

--- dashboard/models.py
+++ dashboard/models.py
@@ -36,5 +36,5 @@ def create_or_update_user_profile(sender, instance, created, **kwargs):
         Create or Update the user profile
     """
     if created:
-        dashboard = Dashboard.objects.create(user=sender.id)
+        dashboard = Dashboard.objects.create(user=instance)
         dashboard.generate_activation_url()
