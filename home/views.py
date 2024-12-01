d6e97d8b7fac6730e37087aedbf5f1d49b61d08f
Antonio Bruchi <antoniobruchi@hotmail.com>
2024-12-01 13:11:23 +0000

    Feat: add carousel with product on clearance on landing page

--- home/views.py
+++ home/views.py
@@ -1,10 +1,19 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-
+from django.conf import settings
+from django.shortcuts import get_object_or_404, render
+from products.models import Product, Tag
 def home(request):
     """view for index page"""
-    
-    print(request.user.id)
-    return render(request, 'home/home.html')
+    products = Product.objects.all()
+    tag = get_object_or_404(
+        Tag, tag='clearance'
+    )
+    tagged_products = []
+    for product in products:
+        for tag_check in product.tags.all():
+            if tag == tag_check:
+                tagged_products.append(product)
+    if len(tagged_products) > 0:
+        context = {
+            'clearance': tagged_products,
+        }
+    return render(request, 'home/home.html', context=context)
